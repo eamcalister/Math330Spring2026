c37d355aa72746a7f3898a25d0f183093c415982
Erich McAlister <65683159+eamcalister@users.noreply.github.com>
2026-01-09 22:07:58 +0000

    Latest build deployed.

---
+++ worksheets/lunr-pretext-search-index.js
@@ -0,0 +1,230 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter",
+  "level": "1",
+  "url": "frontmatter.html",
+  "type": "Front Matter",
+  "number": "",
+  "title": "Front Matter",
+  "body": "  Mathematics Department Fort Lewis College  Last Updated:       "
+},
+{
+  "id": "113-functions",
+  "level": "1",
+  "url": "113-functions.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Math 113 Function Intro Worksheet",
+  "body": " Math 113 Function Intro Worksheet     Use an externally generated image in an exercise.    Use an internally generated table in an exercise.    Use an ordered list for enumeration within an exercise.      In this worksheet we will work with function notation and see a couple Pretext techniques. The exercises are from the active learning ancilliary materials from Functions Modelling Change: A Preparation for Calculus by Connaly et al.       Use the figure below to fill in the missing values.   Graph saved in assets as .png from outside source.                         Data for rainfall, , in Tucson, Arizona is given in below, where time, , is in months with being January.       (months)    (inches)        Find and interpret .      Solve and interpret the meaning of your answer.      Solve and interpret the meaning of your answer.      "
+},
+{
+  "id": "113-functions-2",
+  "level": "2",
+  "url": "113-functions.html#113-functions-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "   Use an externally generated image in an exercise.    Use an internally generated table in an exercise.    Use an ordered list for enumeration within an exercise.    "
+},
+{
+  "id": "113-functions-4-1",
+  "level": "2",
+  "url": "113-functions.html#113-functions-4-1",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Use the figure below to fill in the missing values.   Graph saved in assets as .png from outside source.                    "
+},
+{
+  "id": "113-functions-5-1",
+  "level": "2",
+  "url": "113-functions.html#113-functions-5-1",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Data for rainfall, , in Tucson, Arizona is given in below, where time, , is in months with being January.       (months)    (inches)        Find and interpret .      Solve and interpret the meaning of your answer.      Solve and interpret the meaning of your answer.    "
+},
+{
+  "id": "sec-section-name-5",
+  "level": "1",
+  "url": "sec-section-name-5.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Math 121 Graphical Transformation Worksheet",
+  "body": " Math 121 Graphical Transformation Worksheet     Include an interactive (Desmos) in an activity with a QR code available for the print version.    Note how to include QR codes for interactives in print versions of worksheets.    Discuss creating individual pdf versions of worksheets to hand out in class.      This activity asks students to describe the effect of different affine transformation of functions after experimenting with Desmos. The pre-built Desmos graph appears in the html version while a QR code is created in the print version.     Use the interactive Desmos graph given above with sliders for and to describe the effect of each algebraic substitutions on the graph of .   Note: frhv1grqhj is the last part of the Desmos graph. When you modify the Desmos graph and re-save it, that may change the url.        has the same graph as , except  .       has the same graph as , except  .       has the same graph as , except  .       has the same graph as , except  .       has the same graph as , except  .       has the same graph as , except  .      Interactive Remarks    Obviously, interactive elements don't work very well in printed worksheets. When you print a worksheet you can have it include a static screenshot of the given interactive as well as a QR code to the interactive (still within your pretext book). To do this, you need to edit the file publication-standalone.ptx . In this file, find the html section and edit the baseurl entry to be the url of your deployed Pretext project with \/worksheets at the end.      The ability to print directly from html versions of worksheets isn't great. Hence, if you want to hand out worksheets in class, you will need to do the following:   Uncomment the pretext and article tags at the top and bottom of the worksheet's ptx file.    Run the command pretext build pdf -i .\/source\/activities\/121samplewksht.ptx (change file name to your worksheet) to generate a pdf version and print the worksheet you want.   Now your worksheet will appear in output\/pdfs .      For further discussion of interactive elements, go to .     "
+},
+{
+  "id": "sec-section-name-5-2",
+  "level": "2",
+  "url": "sec-section-name-5.html#sec-section-name-5-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "   Include an interactive (Desmos) in an activity with a QR code available for the print version.    Note how to include QR codes for interactives in print versions of worksheets.    Discuss creating individual pdf versions of worksheets to hand out in class.    "
+},
+{
+  "id": "sec-section-name-5-4",
+  "level": "2",
+  "url": "sec-section-name-5.html#sec-section-name-5-4",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Use the interactive Desmos graph given above with sliders for and to describe the effect of each algebraic substitutions on the graph of .   Note: frhv1grqhj is the last part of the Desmos graph. When you modify the Desmos graph and re-save it, that may change the url.        has the same graph as , except  .       has the same graph as , except  .       has the same graph as , except  .       has the same graph as , except  .       has the same graph as , except  .       has the same graph as , except  .    "
+},
+{
+  "id": "interactive-remarks-2",
+  "level": "2",
+  "url": "sec-section-name-5.html#interactive-remarks-2",
+  "type": "Remark",
+  "number": "4",
+  "title": "",
+  "body": "  Obviously, interactive elements don't work very well in printed worksheets. When you print a worksheet you can have it include a static screenshot of the given interactive as well as a QR code to the interactive (still within your pretext book). To do this, you need to edit the file publication-standalone.ptx . In this file, find the html section and edit the baseurl entry to be the url of your deployed Pretext project with \/worksheets at the end.   "
+},
+{
+  "id": "interactive-remarks-3",
+  "level": "2",
+  "url": "sec-section-name-5.html#interactive-remarks-3",
+  "type": "Remark",
+  "number": "5",
+  "title": "",
+  "body": "  The ability to print directly from html versions of worksheets isn't great. Hence, if you want to hand out worksheets in class, you will need to do the following:   Uncomment the pretext and article tags at the top and bottom of the worksheet's ptx file.    Run the command pretext build pdf -i .\/source\/activities\/121samplewksht.ptx (change file name to your worksheet) to generate a pdf version and print the worksheet you want.   Now your worksheet will appear in output\/pdfs .   "
+},
+{
+  "id": "interactive-remarks-4",
+  "level": "2",
+  "url": "sec-section-name-5.html#interactive-remarks-4",
+  "type": "Remark",
+  "number": "6",
+  "title": "",
+  "body": "  For further discussion of interactive elements, go to .   "
+},
+{
+  "id": "sec-section-name-6",
+  "level": "1",
+  "url": "sec-section-name-6.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Math 221 Tangent Line Worksheet",
+  "body": " Math 221 Tangent Line Worksheet     Show how to include an image using Prefigure code.    Show displayed and alligned math as well as some minor math typesetting issues that come from xml syntax (see the code for details).      In this activity students are asked to decide when to switch the tangent line approximation they use to approximate square roots.    Set-up  Recall from class that we can approximate square roots of numbers that aren't perfect squares using the tangent line approximation near some number  where .  For instance, to approximate we decided to use the tangent line approximation at the nearest perfect square, which is given by  Thus, we can estimate .  Now, what if there isn't a \"nearest\" perfect square? For instance, which tangent line should we use to approximate ( is exactly halfway between and )? The following exercise will help us figure out the answer.     The graph of , along with its tangent lines at and , is given by .   The graph of and its tangent line approximations at and .      a=4  b=9  f(x) = x^(0.5)          (4,2)    f(x) = \\sqrt{x}         (9,3)                   Use to give intervals of -values on which and give more accurate estimates of . That is, find some -value, , such that whenever and whenever .      Use algebra to justify your answer above.      Interactive Remarks    Note the issues in the code where Latex syntax conflicted with xml. There are a few instances when these happen.      Prefigure is really more intuitive to use than Tikz and generates more accessible images. Full documentation is available at and a very convenient tool for testing your figures is available at .     "
+},
+{
+  "id": "sec-section-name-6-2",
+  "level": "2",
+  "url": "sec-section-name-6.html#sec-section-name-6-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "   Show how to include an image using Prefigure code.    Show displayed and alligned math as well as some minor math typesetting issues that come from xml syntax (see the code for details).    "
+},
+{
+  "id": "sec-section-name-6-5",
+  "level": "2",
+  "url": "sec-section-name-6.html#sec-section-name-6-5",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  The graph of , along with its tangent lines at and , is given by .   The graph of and its tangent line approximations at and .      a=4  b=9  f(x) = x^(0.5)          (4,2)    f(x) = \\sqrt{x}         (9,3)                   Use to give intervals of -values on which and give more accurate estimates of . That is, find some -value, , such that whenever and whenever .      Use algebra to justify your answer above.    "
+},
+{
+  "id": "prefigure-math-remarks-2",
+  "level": "2",
+  "url": "sec-section-name-6.html#prefigure-math-remarks-2",
+  "type": "Remark",
+  "number": "8",
+  "title": "",
+  "body": "  Note the issues in the code where Latex syntax conflicted with xml. There are a few instances when these happen.   "
+},
+{
+  "id": "prefigure-math-remarks-3",
+  "level": "2",
+  "url": "sec-section-name-6.html#prefigure-math-remarks-3",
+  "type": "Remark",
+  "number": "9",
+  "title": "",
+  "body": "  Prefigure is really more intuitive to use than Tikz and generates more accessible images. Full documentation is available at and a very convenient tool for testing your figures is available at .   "
+},
+{
+  "id": "sec-section-name-7",
+  "level": "1",
+  "url": "sec-section-name-7.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Math 222 Series Test Worksheet",
+  "body": " Math 222 Series Test Worksheet     Demonstrate a variety of interactive exercises using Calculus II problems (for some reason).      If given as a pre-class activity or uncollected set of exercises in class (so printing would not be necessary), it might make sense to have interactive exercises that check answers and give some sort of feedback. When a print version of this worksheet is generated you get static versions of these problems.    True\/False  vector space   Suppose is a sequence such that . Can you conclude that the series converges?    Check your notes from last class to find a counterexample.    The fact that does not imply necessarily converges. You can apply the integral test to series.      Here's a MyOpenMath problem. They are very easy to include, but perhaps not that attractive.       Here is a Webwork problem, which are also easy to include. (Hoping that a new FLC Webwork server, which will more closely resemble what you see here, will be up by Fall.) Using variants of actual assigned Webwork problems can be helpful in building student confidence when they do their assigned homework. Students must click the \"Activate\" button for the exercise to be interactive.  It is also possible to author problems directly using pg code.     Compute the value of the following improper integral. If it is divergent, type \"Diverges\" or \"D\". Answer:  Using the improper integral from above and the Integral Test, determine whether the following series converges or diverges. Answer \"Converges\" or \"Diverges.\"  Answer:  Converges   Diverges                     Here is an example of a Parson's problem for a proof that a series converges. There are many problem types that can be authored  The problem is to decide if converges.      This series is of the form , where .    Since , this series converges.     , i.e. the sequence of absolute values is positive and decreasing.         Thus, this series converges by the Alternating Series Test.      Interactive Exercise Remarks and Recources   For more documentation on Webwork problems, see (link).   For a general source for sample code, including exercise (other than Webwork), see .   "
+},
+{
+  "id": "sec-section-name-7-2",
+  "level": "2",
+  "url": "sec-section-name-7.html#sec-section-name-7-2",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "   Demonstrate a variety of interactive exercises using Calculus II problems (for some reason).    "
+},
+{
+  "id": "nth-term-divergence",
+  "level": "2",
+  "url": "sec-section-name-7.html#nth-term-divergence",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "True\/False.",
+  "body": " True\/False  vector space   Suppose is a sequence such that . Can you conclude that the series converges?    Check your notes from last class to find a counterexample.    The fact that does not imply necessarily converges. You can apply the integral test to series.   "
+},
+{
+  "id": "sec-section-name-7-5",
+  "level": "2",
+  "url": "sec-section-name-7.html#sec-section-name-7-5",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Here's a MyOpenMath problem. They are very easy to include, but perhaps not that attractive.    "
+},
+{
+  "id": "sec-section-name-7-6",
+  "level": "2",
+  "url": "sec-section-name-7.html#sec-section-name-7-6",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Here is a Webwork problem, which are also easy to include. (Hoping that a new FLC Webwork server, which will more closely resemble what you see here, will be up by Fall.) Using variants of actual assigned Webwork problems can be helpful in building student confidence when they do their assigned homework. Students must click the \"Activate\" button for the exercise to be interactive.  It is also possible to author problems directly using pg code.     Compute the value of the following improper integral. If it is divergent, type \"Diverges\" or \"D\". Answer:  Using the improper integral from above and the Integral Test, determine whether the following series converges or diverges. Answer \"Converges\" or \"Diverges.\"  Answer:  Converges   Diverges                  "
+},
+{
+  "id": "sec-section-name-7-7",
+  "level": "2",
+  "url": "sec-section-name-7.html#sec-section-name-7-7",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  Here is an example of a Parson's problem for a proof that a series converges. There are many problem types that can be authored  The problem is to decide if converges.      This series is of the form , where .    Since , this series converges.     , i.e. the sequence of absolute values is positive and decreasing.         Thus, this series converges by the Alternating Series Test.    "
+},
+{
+  "id": "interactive-exercise-remarks-2",
+  "level": "2",
+  "url": "sec-section-name-7.html#interactive-exercise-remarks-2",
+  "type": "Remark",
+  "number": "10",
+  "title": "",
+  "body": " For more documentation on Webwork problems, see (link).  "
+},
+{
+  "id": "interactive-exercise-remarks-3",
+  "level": "2",
+  "url": "sec-section-name-7.html#interactive-exercise-remarks-3",
+  "type": "Remark",
+  "number": "11",
+  "title": "",
+  "body": "For a general source for sample code, including exercise (other than Webwork), see . "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
